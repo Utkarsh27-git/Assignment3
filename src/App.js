3b0850c3eb1ee715dc3e1ae858043fcdc5267e33
iutkarsh27-solanki <111502301+iutkarsh27-solanki@users.noreply.github.com>
2023-02-15 22:18:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Counterapp from './component/Counterapp'
+
+function App() {
+  return (
+    <div>
+      <Counterapp></Counterapp>
+    </div>
+
+  );
+}
+
+export default App;
